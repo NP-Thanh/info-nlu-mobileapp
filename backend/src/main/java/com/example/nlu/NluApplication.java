c4a10ddc491c258c9f4b7e3afb7fc748aa615c2b
NP-Thanh <22130259@st.hcmuaf.edu.vn>
2026-03-31 23:05:26 +0700

    khoi tao project

---
+++ backend/src/main/java/com/example/nlu/NluApplication.java
@@ -0,0 +1,13 @@
+package com.example.nlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NluApplication.class, args);
+	}
+
+}
